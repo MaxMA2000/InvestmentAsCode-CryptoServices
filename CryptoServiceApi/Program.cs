6e772366528d0ef66e415f7c49fd0b3c34b4637a
MaxMA2000 <maxmahklife@gmail.com>
2024-05-02 22:21:18 +0800

    refactor(IAC-21): refactor CryptoAsset and CryptoPrice into models

--- CryptoServiceApi/Program.cs
+++ CryptoServiceApi/Program.cs
@@ -140,35 +140,35 @@ app.Run();
 
 
 
-public class CryptoAsset
-{
-    public int assetId { get; set; }
-    public string symbol { get; set; }
-    public string name { get; set; }
-    public string exchange { get; set; }
-    public string exchangeShortName { get; set; }
-    public string type { get; set; }
-    public DateTime asOfDate { get; set; }
-}
+// public class CryptoAsset
+// {
+//     public int assetId { get; set; }
+//     public string symbol { get; set; }
+//     public string name { get; set; }
+//     public string exchange { get; set; }
+//     public string exchangeShortName { get; set; }
+//     public string type { get; set; }
+//     public DateTime asOfDate { get; set; }
+// }
 
 
-public class CryptoPrice
-{
-  public long id { get; set; }
-  public long assetId { get; set; }
-  public string symbol { get; set; }
-  public DateTime date { get; set; }
-  public decimal open { get; set; }
-  public decimal high { get; set; }
-  public decimal low { get; set; }
-  public decimal close { get; set; }
-  public decimal adjClose { get; set; }
-  public long volume { get; set; }
-  public long unadjustedVolume { get; set; }
-  public decimal change { get; set; }
-  public decimal changePercent { get; set; }
-  public float vwap { get; set; }
-  public string label { get; set; }
-  public float changeOverTime { get; set; }
-  public string asOfDate { get; set; }
-}
+// public class CryptoPrice
+// {
+//   public long id { get; set; }
+//   public long assetId { get; set; }
+//   public string symbol { get; set; }
+//   public DateTime date { get; set; }
+//   public decimal open { get; set; }
+//   public decimal high { get; set; }
+//   public decimal low { get; set; }
+//   public decimal close { get; set; }
+//   public decimal adjClose { get; set; }
+//   public long volume { get; set; }
+//   public long unadjustedVolume { get; set; }
+//   public decimal change { get; set; }
+//   public decimal changePercent { get; set; }
+//   public float vwap { get; set; }
+//   public string label { get; set; }
+//   public float changeOverTime { get; set; }
+//   public string asOfDate { get; set; }
+// }
