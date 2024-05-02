f65becfccba7b069ee1bd7cfa0a2344030fb02cb
MaxMA2000 <maxmahklife@gmail.com>
2024-05-02 19:50:57 +0800

    feat(IAC-21): create controller `GetSingleDayCryptoPrice`

--- CryptoServiceApi/Program.cs
+++ CryptoServiceApi/Program.cs
@@ -74,6 +74,40 @@ app.MapGet("/v1/crypto/{id}", async (int id) =>
 .WithOpenApi();
 
 
+
+app.MapGet("/v1/crypto/{id}/{date}", async (int id, string date) =>
+{
+    using (var httpClient = new HttpClient())
+    {
+        var apiUrl = $"http://localhost:8080/data/v1/crypto/byAssetIdAndDateRange?asset_id={id}&from={date}&to={date}";
+        var response = await httpClient.GetAsync(apiUrl);
+
+        if (response.IsSuccessStatusCode)
+        {
+            var jsonString = await response.Content.ReadAsStringAsync();
+
+            var jsonArray = JsonSerializer.Deserialize<List<CryptoPrice>>(jsonString);
+
+            if (jsonArray.Count != 1)
+            {
+                return Results.BadRequest("The crypto data is not available or not unique.");
+            }
+
+            var cryptoPrice = jsonArray[0];
+
+            return Results.Json(cryptoPrice);
+        }
+        else
+        {
+            return Results.BadRequest("Failed to retrieve crypto data.");
+        }
+    }
+})
+.WithName("GetSingleDayCryptoPrice")
+.WithOpenApi();
+
+
+
 app.Run();
 
 
@@ -89,3 +123,24 @@ public class CryptoAsset
     public DateTime asOfDate { get; set; }
 }
 
+
+public class CryptoPrice
+{
+  public long id { get; set; }
+  public long assetId { get; set; }
+  public string symbol { get; set; }
+  public DateTime date { get; set; }
+  public decimal open { get; set; }
+  public decimal high { get; set; }
+  public decimal low { get; set; }
+  public decimal close { get; set; }
+  public decimal adjClose { get; set; }
+  public long volume { get; set; }
+  public long unadjustedVolume { get; set; }
+  public decimal change { get; set; }
+  public decimal changePercent { get; set; }
+  public float vwap { get; set; }
+  public string label { get; set; }
+  public float changeOverTime { get; set; }
+  public string asOfDate { get; set; }
+}
